77bdd7a12614a049b2ba3e6e67e6b0d5b53b922a
dimakraft91 <dimakraft91@gmail.com>
2026-02-06 22:22:54 +0300

    ds

--- Новая папка/js/comments.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-  const wrappers = document.querySelectorAll('.fb-comments-wrapper');
-  
-  // Показ всех блоков сразу
-  wrappers.forEach(function(wrapper) {
-      wrapper.style.display = 'block';
-  });
-};
